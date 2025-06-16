c6e855bea42b41fa6e3b5826ab2f6cab76f7d6c8
mhamad raad <hamaraad883@gmail.com>
2025-06-16 23:51:58 +0300

    show the tasks that has been added

--- src/app/components/task-list/task-list.ts
+++ src/app/components/task-list/task-list.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TaskService } from '../../services/task.service';
+import { Task } from '../../models/task.model';
 
 @Component({
+  standalone: true,
   selector: 'app-task-list',
-  imports: [],
   templateUrl: './task-list.html',
-  styleUrl: './task-list.scss'
+  styleUrl: './task-list.scss',
+  imports: [CommonModule],
 })
-export class TaskList {
+export class TaskList implements OnInit {
+  tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.tasks$.subscribe((tasks) => {
+      this.tasks = tasks;
+    });
+  }
+
+  toggle(id: number) {
+    this.taskService.toggleTask(id);
+  }
+
+  delete(id: number) {
+    this.taskService.deleteTask(id);
+  }
 }
