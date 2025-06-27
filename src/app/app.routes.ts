5b161fcd7cc8d5177aa46565ceb869991355201e
mhamad raad <hamaraad883@gmail.com>
2025-06-27 19:48:52 +0300

    solution 04

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,6 +10,7 @@ export const routes: Routes = [
     path: 'task/:id',
     component: TaskDetail,
     data: {
+      renderMode: 'prerender',
       getPrerenderParams: (): Promise<any>[] => [],
     },
   },
