80bd8198dc57aaaf641a2c6d9adc15d49b51fad1
mhamad raad <hamaraad883@gmail.com>
2025-06-27 19:48:04 +0300

    solution 04

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,6 @@ import { TaskList } from './components/task-list/task-list';
 import { AddTask } from './components/add-task/add-task';
 import { TaskDetail } from './components/task-detail/task-detail';
 
-const prerenderTaskIds = ['1', '2', '3'];
-
 export const routes: Routes = [
   { path: '', component: TaskList },
   { path: 'add', component: AddTask },
@@ -12,7 +10,7 @@ export const routes: Routes = [
     path: 'task/:id',
     component: TaskDetail,
     data: {
-      getPrerenderParams: () => prerenderTaskIds.map((id) => ({ id })),
+      getPrerenderParams: (): Promise<any>[] => [],
     },
   },
 ];
