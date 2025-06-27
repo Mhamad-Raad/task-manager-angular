8f022c3ee3cc6909f52f4e91916d4d43f2a58442
mhamad raad <hamaraad883@gmail.com>
2025-06-27 19:21:58 +0300

    solution 01

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { TaskList } from './components/task-list/task-list';
 import { AddTask } from './components/add-task/add-task';
 import { TaskDetail } from './components/task-detail/task-detail';
 
+import { RenderMode } from '@angular/ssr';
+
 export const routes: Routes = [
   { path: '', component: TaskList },
   { path: 'add', component: AddTask },
   {
     path: 'task/:id',
     component: TaskDetail,
-    data: { renderMode: 'no-preference', getPrerenderParams: () => [] },
+    data: { renderMode: RenderMode.Server },
   },
 ];
