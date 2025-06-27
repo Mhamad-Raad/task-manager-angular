26fb5c3f8269a5a079d5ba2fc02f7b922bf88d56
mhamad raad <hamaraad883@gmail.com>
2025-06-27 20:56:43 +0300

    update navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 import { TaskFiltersComponent } from './components/task-filters/task-filters';
+import { TaskService } from './services/task.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, TaskFiltersComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, TaskFiltersComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   standalone: true,
 })
 export class App {
   protected title = 'task-manager';
-}
+  remainingTasks = 0;
 
-// TODO
-//  Reactive Form
-//  API integration
+  constructor(private taskService: TaskService) {
+    this.taskService.tasks$.subscribe((tasks) => {
+      this.remainingTasks = tasks.filter((t) => !t.completed).length;
+    });
+  }
+}
